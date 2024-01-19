37945febe3f9c77a4ea1f216fcbaa3e7931489f8
Efren_Marin <efren45marin@gmail.com>
2024-01-18 16:47:43 -0800

    Set up success and error pages when checking out

---
+++ pages/success.js
@@ -0,0 +1,44 @@
+import { CheckCircleIcon } from "@heroicons/react/24/solid";
+import axios from "axios";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import { useShoppingCart } from "use-shopping-cart";
+
+export default function SuccessPage() {
+    const { clearCart } = useShoppingCart()
+	const router = useRouter();
+	const sessionID = router.query.session_id;
+	const { data, error } = useSWR(
+		() => (sessionID ? `/api/checkout-sessions/${sessionID}` : null),
+		(url) => axios.get(url).then((res) => res.data),
+		{
+			onSuccess() {
+                clearCart()
+            },
+		}
+	);
+	const email = data?.customer_details.email;
+	return (
+		<div className='container xl:max-w-screen-xl mx-auto py-12 px-6 text-center'>
+			{error ? (
+				<div className='p-2 rounded-md bg-rose-100 text-rose-500 max-w-md mx-auto'>
+					<p className='text-lg'>Sorry, we hit a snag!</p>
+				</div>
+			) : !data ? (
+				<div className='p-2 rounded-md text-gray-500 max-w-md mx-auto'>
+					<p className='text-lg'>Loading...</p>
+				</div>
+			) : (
+				<div className='py-4 px-8 space-y-4 rounded-md max-w-lg mx-auto'>
+					<CheckCircleIcon className='w-24 h-24 mx-auto flex-shrink-0 text-lime-600' />
+					<h2 className='text-4xl font-semibold flex flex-col items-center space-x-1'>
+						Thanks for your order!
+					</h2>
+					<p className='text-lg'>
+						Check your email ({email}) for your receipt.
+					</p>
+				</div>
+			)}
+		</div>
+	);
+}
