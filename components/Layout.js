37def5a1dd1dc89f897fd28174e0687ac45e24a7
Efren_Marin <efren45marin@gmail.com>
2024-01-18 17:00:32 -0800

    Changed default font

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,16 @@
+import { Inter } from 'next/font/google'
 import Footer from "./Footer";
 import Nav from "./Nav";
 
+
+const inter = Inter({
+	subsets: ['latin'],
+	variable: "--font-inter"
+})
+
 const AppLayout = ({ children }) => {
 	return (
-		<div className='min-h-screen flex flex-col'>
+		<div className={`${inter.variable} font-sans min-h-screen flex flex-col`}>
 			<main className='flex-grow bg-[#f7f7f7]'>
 				<Nav />
 				{children}
